00304fdda925ae861c7a2809bf251fdaa661a97f
divya <divya@gmail.com>
2026-02-21 10:27:17 +0530

    modified resource name by divya

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "web" {
   ami           = "ami-051a31ab2f4d498f5"
   instance_type = "t3.micro"
   tags = {
-    Name = "tf-cloud-instance"
+    Name = "tf-cloud-inst-modified"
   }
 }
 
